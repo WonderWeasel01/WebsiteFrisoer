6581bc3f92ae3a9a7dc555e091428ea8cb1ccd5b
Wonder <69655111+WonderWeasel01@users.noreply.github.com>
2023-02-20 11:33:56 +0100

    main

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "admin" && password === "password") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
