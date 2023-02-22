de8efe13d1e349f597b9ffdc3f4e6c791176d23e
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-02-22 02:26:36 +0100

    Work in progress

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "admin" && password === "password") {
+    if (username === "dennis" && password === "umaru") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
